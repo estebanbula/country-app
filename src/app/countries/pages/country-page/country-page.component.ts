000f5992ddc011dd48a63f080781a2c8582eed69
Esteban Bula <carlosbula01@gmail.com>
2023-07-18 18:34:52 -0500

    Country detail page

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {CountriesService} from "../../services/countries.service";
+import {map, switchMap} from "rxjs";
+import {Country} from "../../interfaces/country.interface";
 
 @Component({
   selector: 'country-page',
@@ -6,6 +10,29 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  public country?: Country;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private _countriesService: CountriesService,
+    private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({id}) => this._countriesService.searchCountryByAlfaCode(id))
+      )
+      .subscribe(country => country ? this.setCountry(country) : null);
+  }
+
+  private setCountry(country: Country): void {
+    if (!country) {
+      this.router.navigateByUrl("").then(r => r.valueOf());
+    }
+    this.country = country;
+  }
 
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {catchError, Observable, of} from "rxjs";
+import {catchError, map, Observable, of} from "rxjs";
 import {Country} from "../interfaces/country.interface";
 
 @Injectable({
@@ -11,6 +11,17 @@ export class CountriesService {
   private apiUrl: string = "https://restcountries.com/v3.1";
 
   constructor(private httpClient: HttpClient) { }
+
+  public searchCountryByAlfaCode(code: string): Observable<Country | null> {
+    const headers: HttpHeaders = new HttpHeaders();
+    headers.set("Access-Control-Allow-Origin", "*");
+    return this.httpClient
+      .get<Country[]>(`${this.apiUrl}/alpha/${code}`, {headers})
+      .pipe(
+        map(countries => countries.length > 0 ? countries[0] : null),
+        catchError(error => of(null))
+      );
+  }
 
   public searchByCapital(term: string): Observable<Country[]> {
     const headers: HttpHeaders = new HttpHeaders();
